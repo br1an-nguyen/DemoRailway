ed202aa03690c192f8978c127fbaa8be5f2858c7
Nguyen Quang Minh <23127089@student.hcmus.edu.vn>
2025-10-08 22:59:56 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World from Node.js via ngrok');
 });
 
-const port = 3000;
-app.listen(port);
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => console.log('on', port));
 console.log(`Server is listening on port ${port}`);
+
